f12fb42169890b82a9e28c7980adab9d00467d9b
Michael Vinson <MVinsonCPNB@gmail.com>
2018-07-21 12:21:58 -0400

    add integration test and configured routes and links. Bookmark continue 5.4 User Signup

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+
+  # using the get '/help', to: 'static_pages#help'
+  # rails creates to paths the 
+  # help_path -> '/help'  AND THE help_url -> 'http://www.example.com/help'
+
+  get '/help',       to: 'static_pages#help'
+  get '/about',      to: 'static_pages#about'
+  get '/contact',    to: 'static_pages#contact'
+
+
+  # Note: get 'static_pages/help' make a symbol avaialble
+  # such as, static_pages_help_url
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -2,19 +2,25 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url # use a get request and the url
+    get root_path # use a get request and the url
     assert_response :success  # show true if the response is valid
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
-      get static_pages_about_url
+      get about_path
       assert_response :success
       assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+      get contact_path
+      assert_response :success
+      assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
