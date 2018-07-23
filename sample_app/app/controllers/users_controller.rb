157eda0cb5ef7d681cc31962798fbbe424417fe0
Michael Vinson <MVinsonCPNB@gmail.com>
2018-07-23 16:02:53 -0400

    BM 7.4.3 start at the TODO

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
  
     def show
-        @user = User.find(params[:id])
+        #TODO the redirect to show from the create method tries to 
+        #access user_params, and user_params shows there is no 
+        #paramert passed to this method, either create a gloable 
+        #call method set within user_params or within user.save true
+        #if statment section
+        @user = User.find(@user.id)
 
         # the dubugger opens a active debuggin console 
         # in the rails server terminal used to view
@@ -16,16 +21,18 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)     # Not the final implementation!
-
+        
         if @user.save
-            # Handle a successful save.
+            flash[:success] = "Welcome to the Sample App!"
+            # (BELOW) equivalent redirect_to user_url(@user)
+            redirect_to @user
         else
             render 'new'
         end
     end
 
-    private 
         
+    private 
         def user_params
             params.require(:user).permit(:name, :email,
                                          :password,
